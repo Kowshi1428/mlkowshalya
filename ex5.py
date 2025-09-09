4c954c7e2acda155fa0fdb0cab0cedf5102e7613
Kowshi1428 <kowshalya1428@gmail.com>
2025-09-09 12:11:08 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+data={
+    'age':[25,30,35,40,45,50,55,60]
+                
